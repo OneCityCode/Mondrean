cc7beed52a9e4031dfbbf68c925c9b0616490d63
Matthew <matthew.phillip@gmail.com>
2023-08-25 17:23:44 -0400

    First Commit

---
+++ Streamlit/test.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from streamlit_card import card
+from PIL import Image
+
+
+img = Image.open('card.jpg')
+
+st.image(img)
+
+# card(
+#     title="Minute ON Demand REview ANalysis",
+#     text="Some description",
+#     image=('Card.jpg'),
+#     url="https://www.google.com",
+# )
