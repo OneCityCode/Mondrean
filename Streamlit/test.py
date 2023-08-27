76f2759551fcb965cf1b706099301bd4d969d83c
Matthew <matthew.phillip@gmail.com>
2023-08-27 11:41:14 -0400

    Tile with base64

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -2,13 +2,13 @@ import streamlit as st
 # import pandas as pd
 # import numpy as np
 from PIL import Image
-# # import base64
+import base64
 
 
-# with open("./Images/Title.png", "rb") as f:
-#     data = f.read()
-#     encoded = base64.b64encode(data)
-# data = "data:image/png;base64," + encoded.decode("utf-8")
+with open("./Images/Title.png", "rb") as f:
+    data = f.read()
+    encoded = base64.b64encode(data)
+data = "data:image/png;base64," + encoded.decode("utf-8")
 
 Image.open("./Images/Title.png")
 
