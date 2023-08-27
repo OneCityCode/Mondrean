185d28a0c68bd88038ce5fce7696e03b7f7c4dae
Matthew <matthew.phillip@gmail.com>
2023-08-27 13:25:50 -0400

    added text input

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -2,7 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(layout="wide", page_title="Mondrean", page_icon="🖌️")
+st.set_page_config(layout="centered", page_title="Mondrean", page_icon="🖌️")
 
-st.image("https://raw.githubusercontent.com/OneCityCode/Test/main/Streamlit/Title.png")
+with st.container():
+    st.image("https://raw.githubusercontent.com/OneCityCode/Test/main/Streamlit/Title.png")
+
+    st.text_input("Enter the name of the product you would like to learn about", key="productname")
+
+    st.session_state.productname
+
+
+
 
