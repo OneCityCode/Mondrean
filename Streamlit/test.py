1aa14567c914dce8a26fe61dcfa17958abcd45f5
Matthew <matthew.phillip@gmail.com>
2023-08-27 11:43:27 -0400

    Title moved to main

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,16 +1,16 @@
 import streamlit as st
 # import pandas as pd
 # import numpy as np
-from PIL import Image
-import base64
+# from PIL import Image
+# import base64
 
 
-with open("./Images/Title.png", "rb") as f:
-    data = f.read()
-    encoded = base64.b64encode(data)
-data = "data:image/png;base64," + encoded.decode("utf-8")
+# with open("Title.png", "rb") as f:
+#     data = f.read()
+#     encoded = base64.b64encode(data)
+# data = "data:image/png;base64," + encoded.decode("utf-8")
 
-Image.open("./Images/Title.png")
+# Image.open("Title.png")
 
-st.image("./Images/Title.png")
+st.image("Title.png")
 
