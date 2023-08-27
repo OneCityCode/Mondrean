a2edc100ef357d707b218cc3f4048ec9cbbe28c9
Matthew <matthew.phillip@gmail.com>
2023-08-27 10:29:26 -0400

    Working on local

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -3,15 +3,28 @@ import pandas as pd
 import numpy as np
 from streamlit_card import card
 from PIL import Image
+import base64
 
 
-Image.open('./Images/Cardm.jpeg')
+with open("./Images/Cardm.jpeg", "rb") as f:
+    data = f.read()
+    encoded = base64.b64encode(data)
+data = "data:image/png;base64," + encoded.decode("utf-8")
+
+# Image.open('./Images/Cardm.jpeg')
 
 # st.image.show(image)
 
 card(
-    title="Minute ON Demand REview ANalysis",
-    text="Some description",
-    image='./Images/Cardm.jpeg',
-    url="https://www.google.com",
+    title="Mondrean",
+    text="Minute ON Demand REview ANalysis",
+    image=data,
+    styles={
+        "card": {
+            "width": "500px",
+            "height": "300px",
+            "border-radius": "10px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",}
+        },
+
 )
