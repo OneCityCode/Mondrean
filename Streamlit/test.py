0800afce8b97d025dfca867a990fccf21f0efaf5
Matthew <matthew.phillip@gmail.com>
2023-08-27 11:37:57 -0400

    Working locally, Title image

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,30 +1,16 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from streamlit_card import card
-from PIL import Image
-import base64
+# import pandas as pd
+# import numpy as np
+# # from PIL import Image
+# # import base64
 
 
-with open("./Images/Cardm.jpeg", "rb") as f:
-    data = f.read()
-    encoded = base64.b64encode(data)
-data = "data:image/png;base64," + encoded.decode("utf-8")
+# with open("./Images/Title.png", "rb") as f:
+#     data = f.read()
+#     encoded = base64.b64encode(data)
+# data = "data:image/png;base64," + encoded.decode("utf-8")
 
-# Image.open('./Images/Cardm.jpeg')
+# Image.open("./Images/Title.png")
 
-# st.image.show(image)
+st.image("./Images/Title.png")
 
-card(
-    title="Mondrean",
-    text="Minute ON Demand REview ANalysis",
-    image=data,
-    styles={
-        "card": {
-            "width": "500px",
-            "height": "300px",
-            "border-radius": "10px",
-            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",}
-        },
-
-)
