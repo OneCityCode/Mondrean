690624c05d0dc1269082c0dea1dc30ad725cb0c7
Matthew <matthew.phillip@gmail.com>
2023-08-27 11:44:27 -0400

    Title with PIL

--- Streamlit/test.py
+++ Streamlit/test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 # import pandas as pd
 # import numpy as np
-# from PIL import Image
+from PIL import Image
 # import base64
 
 
@@ -10,7 +10,7 @@ import streamlit as st
 #     encoded = base64.b64encode(data)
 # data = "data:image/png;base64," + encoded.decode("utf-8")
 
-# Image.open("Title.png")
+Image.open("Title.png")
 
 st.image("Title.png")
 
